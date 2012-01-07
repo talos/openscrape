720a9be0c35f3aef202a2d762e1b372b03a867f7
talos <irving.krauss@gmail.com>
2012-01-06 21:18:12 -0500

    start.sh boots everything up, http_receiver mapping correctly

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-java -Djava.library.path=/usr/local/lib -classpath /Users/talos/Programming/jzmq/src/ -jar server/caustic_backend.jar
+#!/bin/bash
+
+# start up the java backend
+java -Djava.library.path=/usr/local/lib -classpath /Users/talos/Programming/jzmq/src/ -jar server/caustic_backend.jar > log/caustic_backend.log 2>&1 &
+
+# start up mongrel
+m2sh load -config mongrel2.conf -db the.db
+m2sh start -db the.db -host localhost > log/mongrel2.log 2>&1 &
+
+# spin up brubeck
+python server/http_receiver.py > log/http_receiver.log 2>&1 &
