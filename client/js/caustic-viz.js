41d70275d82d342432e9656bb3ad77a10ea54337
talos <irving.krauss@gmail.com>
2012-01-09 13:07:43 -0500

    working from new response format.  not yet displaying visuals, though.

--- client/js/caustic-viz.js
+++ client/js/caustic-viz.js
@@ -1,3 +1,10 @@
+/**
+   Depends upon jQuery (1.7.1), Underscore (1.2.4), d3 core, d3
+   layout, and json2.
+
+   Licensed under the GNU GPL v3.
+**/
+
 $(document).ready(function() {
     /*******************
 
@@ -10,26 +17,24 @@ $(document).ready(function() {
     /** Path to hit caustic backend. **/
     var request_path = "/request",
 
-    _observe, // TODO better system of controlling observed root ID
-
-    _data = {}, // TODO move this to some kind of page data store
+    _data = {}, // TODO move this to some kind of page or object data store
 
     /**
-       Put tags for ID in generic data.  Will overwrite data.
+       Store generic data. Uses combiner to modify existing data.
 
+       @param combiner A nondestructive function returning combined
+       data.  Will be passed the old value as its first argument, and
+       the new value as its second.
        @param key The key of the store to use for persistence.
-       @param combine A two-argument nondestructive function returning combined data.
        @param id The String ID to put tags into.
-       @param newData The data to add.
+       @param value The value to put.
     **/
-    _put = function(key, combine, id, newData) {
+    _put = function(combiner, key, id, value) {
         if(!_data.hasOwnProperty(key)) {
             _data[key] = {};
         }
-        _data[key][id] = combine(_data[key][id], newData);
-        //console.log(_data[key]);
-        //console.log(_data);
-        redraw();
+        _data[key][id] = combiner(_get(key, id), value);
+        //redraw();
     },
 
     /**
@@ -44,96 +49,103 @@ $(document).ready(function() {
         return _data.hasOwnProperty(key) ? _data[key][id] : undefined;
     },
 
-    // Curry functions for relationships.
-    _replace = function(existing, added) {
-        return added;
-    },
-    _append = function(existing, newElem) {
-        var ary = $.makeArray(existing);
-        ary.push(newElem);
-        return ary;
-    },
+    /**
+       Replace an old value with a new value when used as the combiner in _put().
+
+       @param oldVal the old value.
+       @param newVal the new value.
+     **/
+    _replace = function(oldVal, newVal) {
+        return newVal;
+    }
+
     /**
        Generate a new ID.
 
-       @param key The String key spawning this new ID.
-       @param value The String value spawning this new ID.
        @param parentId The parent ID, can be omitted if this is root.
 
        @return The new ID.
      **/
-    newId = function(key, value, parentId) {
-        this.id = this.id === undefined ? 0 : ++this.id; // ftw
-
+    newId = function(parentId) {
+        id = _.uniqueId();
         if(parentId === null || typeof parentId === 'undefined') {
-            _observe = id; // TODO this is hacky, but must be called before redraw() (which is triggered by _put('id,...)
+            _put(_replace, 'parent', id, parentId);
+            //_put('children', _extend_merge, parentId, obj);
         }
 
-        var obj = {};
-        obj[key] = this.id;
-        _put('id', _replace, this.id, [key, value]);
-        if(parentId !== null && typeof parentId !== 'undefined') {
-            _put('parent', _replace, parentId, this.id);
-            _put('children', _extend_merge, parentId, obj);
-        }
-        return this.id;
+        return id;
     },
-    getIdKeyValue = function(id) {
-        return _get('id', id);
-    },
+    // getIdKeyValue = function(id) {
+    //     return _get('id', id);
+    // },
     getParent = function(id) {
         return _get('parent', id);
     },
-    getChildren = function(id) {
-        return _get('children', id);
-    },
+    // getChildren = function(id) {
+    //     return _get('children', id);
+    // },
+
+    getResponse = function(id) {
+        return _get('response', id);
+    }
 
     /**
-       Extract data for ID from generic persistence.  Ascends tree.
+       Iterate up the ID tree.
 
-       @param key The key of the store used for persistence.
-       @param combine A two-argument nondestructive function returning combined data.
+       @param iterator A function that will be passed each value up
+       the tree in turn, with the result of the last iteration as the
+       first argument, and the parent ID as the second.
+       @param memo An initial value for iterator's first argument.
        @param id the String ID to get data for.
+
+       @return The result of iterator's last execution.
     **/
-    _getAll = function(key, combine, id) {
-        var result = {};
+    ascend = function(iterator, memo, id) {
         do {
-            result = combine(_get(key, id), result);
-        } while((id = getParent(key, id)) !== undefined);
-        return result;
+            memo = iterator(memo, id);
+        } while((id = getParent(id)) !== undefined);
+        return memo;
     },
 
-    // Curry functions for tags.
-    _extend = function(parent, child) {
-        return $.extend({}, parent, child); // We extend this direction to prefer child data.
-    },
-    putTags = function(id, tags) {
-        return _put('tags', _extend, id, tags);
-    },
     getTags = function(id) {
-        return _getAll('tags', _extend, id);
+        return ascend(function(memo, id) {
+            var resp = _get('response', id),
+            tags = _get('tags', id);
+            if(!_.isUndefined(tags)) { // Special tags may not be defined for every ID
+                // prefer child values
+                memo = _.extend({}, tags, memo);
+            }
+            if(!_.isUndefined(resp)) {
+                if(resp.children.length == 1) {
+                    var name = resp.name,
+                    value = resp.children[0].name;
+                    // don't overwrite properties -- this means we prefer child values
+                    if(!memo.hasOwnProperty(name)) {
+                        memo[name] = value;
+                    }
+                }
+            }
+            return memo;
+        }, {}, 'response', id);
     },
 
-    // Curry functions for cookies.
-    /**
-       Return new JS obj hash of arrays containing arrays appended
-       from parent and child.
-
-       parent A JS obj hash of arrays.
-       child A JS obj hash of arrays.
-     **/
-    _extend_merge = function(parent, child) {
-        var combined = $.extend({}, parent);
-        $.each(child, function(k, v) {
-            combined[k] = $.merge($.makeArray(combined[k]), $.makeArray(v));
-        });
-        return combined;
-    },
-    putCookies = function(id, cookies) {
-        return _put('cookies', _extend_merge, id, cookies);
-    },
     getCookies = function(id) {
-        return _getAll('cookies', _extend_merge, id);
+        return ascend(function(memo, id) {
+            var resp = _get('response', id);
+            if(!_.isUndefined(resp)) {
+                if(resp.hasOwnProperty('cookie')) {
+                    _.each(resp.cookies, function(host, cookiesAry) {
+                        // merge array for host if it already exists.
+                        if(memo.hasOwnProperty(host)) {
+                            memo.host = memo.host.concat(cookiesAry);
+                        } else {
+                            memo.host = cookiesAry;
+                        }
+                    });
+                }
+            }
+            return memo;
+        }, {}, 'response', id);
     },
 
     /**
@@ -148,15 +160,17 @@ $(document).ready(function() {
     /**
        Make a forced request for <code>instruction</code>.
 
+       @param $queue A jQuery DOM object to queue under.
        @param id The String ID of the request.  Used to get tags.
        @param instruction A String instruction.
        @param force Whether to force a load.
        @param uri URI to resolve instruction references against.  Optional.
        @param input Input String.  Optional.
     **/
-    request = function(id, instruction, force, uri, input) {
-        this.queue = this.queue === undefined ? new $.Deferred().resolve() : this.queue;
-        this.queue = this.queue.pipe(
+    request = function($queue, id, instruction, force, uri, input) {
+        console.log('added to queue');
+        $queue.queue('caustic', function() {
+            console.log('executing queue');
             $.post(request_path,
                    JSON.stringify({
                        "id": id,
@@ -166,87 +180,71 @@ $(document).ready(function() {
                        "tags" : getTags(id),
                        "input": input, // Stringify drops this key if undefined.
                        "force": String(force)}))
-                .done(handleResponse)
+                .done(function(resp) {
+                    saveResponse(JSON.parse(resp), id);
+                })
                 .fail(function(resp) {
                     warn("Request for " + instruction + " failed: " + resp.statusText);
-                }));
-        //console.log(this.queue.state());
+                })
+                .always(function() {
+                    $queue.dequeue('caustic');
+                })
+        });
+        console.log($queue.queue('caustic').length);
+        if($queue.queue('caustic').length == 1) {
+            $queue.dequeue('caustic');
+        }
+        console.log($queue.queue('caustic').length);
     },
 
     /**
-       Handle a response from {@link #request}.
+       Save tags independent of other data.
 
-       @param json The raw JSON response.
+       @param id The ID to associate with the tags.
+       @param tags A JS hash of tags to save.  Should be String-String.
+     **/
+    saveTags = function(id, tags) {
+        _put(_replace, 'tags', id, tags); // TODO: should not replace existing tags
+    },
+
+    /**
+       Save a response from {@link #request}.
+
+       @param id Internal ID used for tags.  Different from the ID
+       returned in the response.
+       @param resp The response as a JS object.
     **/
-    handleResponse = function(json) {
-        var resp = JSON.parse(json),
-        id = resp['id'],
-        childInputs = [],
-        childIds = [];
-
-        if(resp.hasOwnProperty('cookies')) {
-            putCookies(id, resp['cookies']);
-        }
-
-        if(resp.hasOwnProperty('values')) {
-            // If there were multiple values passed back, create an ID
-            // for each child -- it's a branch.
-            if(resp['values'].length > 1) {
-                $.each(resp['values'], function(idx, value) {
-                    var childId = newId(resp['name'], value, id),
-                    tagsObj = {};
-
-                    childIds.push(childId);
-                    childInputs.push(value);
-                    tagsObj[resp['name']] = value;
-
-                    putTags(childId, tagsObj);
-                });
-            } else { // Otherwise, stick value in with existing ID.
-                var tagsObj = {};
-                tagsObj[resp['name']] = resp['values'][0];
-                childIds.push(id);
-                childInputs.push(resp['values'][0]);
-                putTags(id, tagsObj);
-            }
-
-            // TODO: retry missing tags if affected
-        }
-
-        // If content was passed back, children should be executed in this ID.
-        if(resp.hasOwnProperty('content')) {
-            childIds.push(id);
-            childInputs.push(resp['content']);
-        }
-
+    saveResponse = function(id, resp) {
         if(resp.hasOwnProperty('children')) {
-            $.each(resp['children'], function(idx, childInstruction) {
-                $.each(childIds, function(idx, childId) {
-                    request(childId,
-                            childInstruction,
-                            false,
-                            resp['uri'],
-                            childInputs[idx]); // TODO: this is a
-                                               // messy way of
-                                               // handling child
-                                               // inputs, as the two
-                                               // arrays happen to be
-                                               // the same size
+            // children are returned in a map between input values and
+            // full responses.  This is converted into an array with
+            // objects in the format of:
+            // {
+            //    name: <key of map>,
+            //    children: [<id>, <id>, <id>,...]
+            // }
+            // where the ID is newly generated, and can be used to get
+            // the response that was there.
+            var ary;
+            _.each(resp.children, function(name, respArray) {
+                childIds = []
+                _.each(respArray, function(idx, childResp) {
+                    var childId = newId(id);
+                    saveResponse(childId, childResp);
+                    childIds.push(childId);
+                });
+                ary.push({
+                    name: name,
+                    children: childIds
                 });
             });
+            resp.children = ary;
+        } else {
+            resp.children = []
         }
 
-        if(resp.hasOwnProperty('failed')) {
-            // TODO: store failed
-            console.log("Request failed: " + resp['failed']);
-        }
-
-        if(resp.hasOwnProperty('missing')) {
-            // TODO: store missing tags
-            console.log("TODO retry missing tags: " + resp['missing']);
-        }
-
-        //console.log(resp);
+        _put(_replace, 'response', id, resp);
+        redraw(id);
     };
 
     /**
@@ -255,9 +253,11 @@ $(document).ready(function() {
     $('div#request form').submit(function() {
         try {
             var instruction = $(this).find('#instruction').val(),
-            id = newId('Root', instruction);
-            putTags(id, JSON.parse($(this).find('#tags').val()));
-            request(id, instruction, true);
+            id = newId();
+            saveTags(id, JSON.parse($(this).find('#tags').val()));
+
+            // TODO should select the lock object dynamically
+            request($('#visuals'), id, instruction, true);
         } catch(err) {
             if(err instanceof SyntaxError) {
                 warn("Bad JSON for tags: " + err.message);
@@ -275,53 +275,26 @@ $(document).ready(function() {
 
     *********************/
 
-
-    /**
-       Construct a hierarchical JS object suitable for visualization.
-
-       @param id The root ID of the visualized object.
-
-       @return A JS object.
-     **/
-    var constructData = function(id) {
-        var name = getIdKeyValue(id)[1],
-        obj = {
-            name : name,  // TODO not ideal
-            children : []
-        },
-        tags = _get('tags', id), // pull out immediate tags.
-        children = getChildren(id);
-        if(tags !== null && typeof tags !== 'undefined') {
-            $.each(tags, function(name, value) {
-                obj.children.push({
-                    name:  name,
-                    value: value
-                });
-            });
-        }
-
-        // recursively add 'real' children
-        if(children !== null && typeof children !== 'undefined') {
-            $.each(children, function(key, childrenByKey) {
-                var childrenByKeyObj = {
-                    name : key,
-                    children : []
-                };
-                $.each(childrenByKey, function(idx, childId) {
-                    childrenByKeyObj.children.push(constructData(childId));
-                });
-                obj.children.push(childrenByKeyObj);
-            });
-        }
-
-        return obj;
-    };
-
     var w = 800,
     h = 600;
 
     var cluster = d3.layout.cluster()
-        .size([h, w - 160]);
+        .size([h, w - 160])
+        .children(function(d) {
+            if(d.hasOwnProperty('id')) {
+                // If data has an id, it is a response. Its children
+                // are child nodes that can be returned directly.
+                return d.children;
+            } else {
+                // Otherwise it is a child node, its children need to be
+                // expanded from IDs into responses.
+                var children = [];
+                _.each(d.children, function(idx, childId) {
+                    children.push(getResponse(childId));
+                });
+                return children;
+            }
+        });
 
     var diagonal = d3.svg.diagonal()
         .projection(function(d) { return [d.y, d.x]; });
@@ -332,11 +305,13 @@ $(document).ready(function() {
         .append("g")
         .attr("transform", "translate(40, 0)");
 
+    /**
+       Redraw the node with id.
 
-    var redraw = function() {
-
-        // TODO for now, all redraws are done from _observe as root.
-        var nodes = cluster.nodes(constructData(_observe));
+       @param id the ID of the node to redraw.
+     **/
+    var redraw = function(id) {
+        var nodes = cluster.nodes(getResponse(id));
 
         var link = vis.selectAll("path.link")
             .data(cluster.links(nodes));
