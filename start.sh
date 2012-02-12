32bd011566816fbc2b863e18c5371f73b4ca2200
talos <irving.krauss@gmail.com>
2012-02-12 17:30:51 -0500

    have to specify mode for proxy

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 java -Djava.library.path=/usr/local/lib -classpath /Users/talos/Programming/jzmq/src/ -jar server/caustic_backend.jar > log/caustic_backend.log 2>&1 &
 
 # spin up brubeck
-python server/proxy.py > log/proxy.log 2>&1 &
+python server/proxy.py production > log/proxy.log 2>&1 &
