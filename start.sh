c4cfc3f9d7a5905cd6f5cc1ca4c6af57ba13c0ac
talos <irving.krauss@gmail.com>
2012-02-12 17:08:25 -0500

    removing mongrel references from start

--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 # start up the java backend
-#java -Djava.library.path=/usr/local/lib -classpath /Users/talos/Programming/jzmq/src/ -jar server/caustic_backend.jar > log/caustic_backend.log 2>&1 &
-
-# start up mongrel
-m2sh load
-m2sh start -name caustic -sudo
+java -Djava.library.path=/usr/local/lib -classpath /Users/talos/Programming/jzmq/src/ -jar server/caustic_backend.jar > log/caustic_backend.log 2>&1 &
 
 # spin up brubeck
-#workon caustic
-#python server/http_receiver.py > log/http_receiver.log 2>&1 &
-
-ps ax | grep java
-ps ax | grep mong
-ps ax | grep python
+python server/proxy.py > log/proxy.log 2>&1 &
