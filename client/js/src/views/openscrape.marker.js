11ff495130828960ddd1fb6172bd4715afbc4feb
talos <irving.krauss@gmail.com>
2012-02-13 18:11:11 -0500

    eliminating forcestopdrag leftovers

--- client/js/src/views/openscrape.marker.js
+++ client/js/src/views/openscrape.marker.js
@@ -96,6 +96,7 @@ define([
                             }).render();
                             this.model.destroy();
                         } else {
+                            //this.model.setAddress(address);
                             var rootNode = this.model.nodes.create({
                                 tags: address,
                                 instruction: 'instructions/nyc/property.json',
